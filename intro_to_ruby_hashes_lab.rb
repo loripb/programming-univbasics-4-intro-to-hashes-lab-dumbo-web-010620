732529cc6cbb75c4502e31b93c1637447cc4f253
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-13 08:39:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash.default = 1
+  hash.default(1)
 
   hash[key]
   # given a hash an a key as parameters, return an updated hash
