167297fd24398c971ed29352c678b6b96e1611ae
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-13 08:58:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash.defalt(1)
-  if hash[key]
+  hash.defalt = 1
+  if hash.has_key?(key)
     hash[key] += 1
   end
   hash
